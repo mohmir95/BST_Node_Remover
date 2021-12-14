cc59ef64dc16f38de5d0dd1149eaa24fd4a9e713
MohMir <mohmir95@gmail.com>
2021-12-14 11:21:07 +0330

    first commit

---
+++ BST_Node_Remover/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BST_Node_Remover
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
